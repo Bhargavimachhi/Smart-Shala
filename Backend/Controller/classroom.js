a0d724848033f94db576a1435d7eb25a231538e0
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2024-11-22 00:58:29 +0530

    Added routes to assign teacher and student to classroom

--- Backend/Controller/classroom.js
+++ Backend/Controller/classroom.js
@@ -45,3 +45,63 @@ export const getAllClassrooms = async(req,res)=>{
         res.status(500).json({message:"internal server error"})
     }
 }
+
+//assign teacher
+export const assignTeacher = async(req,res) => {
+    let teachers = await Teacher.find({email : req.body.email});
+    let classroom = await Classroom.find({_id : req.body.id});
+
+    if(teachers.length == 0) {
+        res.status(404).json({"message" : "Teacher does not exist"});
+        return;
+    }
+
+    if(classroom.length == 0) {
+        res.status(404).json({"message" : "Classroom does not exist"});
+        return;
+    }
+
+    if(classroom[0].teachers.includes(teachers[0]._id)) {
+        res.status(404).json({"message" : "Teacher already exist"});
+        return;
+    }
+
+    classroom[0].teachers.push(teachers[0]._id);
+
+    classroom[0].save().then(()=>{
+        res.status(200).json({ message: "Teacher Assigned to Classroom Successfully" });
+    }).catch((err)=>{
+        console.log(err);
+        res.send("Error Occurred !!! , Couldn't Assign teacher to the classroom");
+    });
+}
+
+//assign student
+export const assignStudent = async(req,res) => {
+    let students = await Student.find({email : req.body.email});
+    let classroom = await Classroom.find({_id : req.body.id});
+
+    if(students.length == 0) {
+        res.status(404).json({"message" : "Student does not exist"});
+        return;
+    }
+
+    if(classroom.length == 0) {
+        res.status(404).json({"message" : "Classroom does not exist"});
+        return;
+    }
+
+    if(classroom[0].students.includes(students[0]._id)) {
+        res.status(404).json({"message" : "Student already exist"});
+        return;
+    }
+
+    classroom[0].students.push(students[0]._id);
+
+    classroom[0].save().then(()=>{
+        res.status(200).json({ message: "Student Assigned to Classroom Successfully" });
+    }).catch((err)=>{
+        console.log(err);
+        res.send("Error Occurred !!! , Couldn't Assign student to the classroom");
+    });
+}
