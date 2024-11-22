584edaf9918ddea572ff711ec40d1cb379b68828
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2024-11-22 22:17:54 +0530

    changed classroom preview data

--- src/Pages/Admin/Pages/ClassroomListing.jsx
+++ src/Pages/Admin/Pages/ClassroomListing.jsx
@@ -88,7 +88,8 @@ const ClassroomListingpage = () => {
                             <thead>
                               <tr className="bg-gray-100">
                                 <th className="py-2 px-4 text-left">Name</th>
-                                <th className="py-2 px-4 text-left">Roll No</th>
+                                <th className="py-2 px-4 text-left">Roll no</th>
+                                <th className="py-2 px-4 text-left">Contact</th>
                               </tr>
                             </thead>
                             <tbody>
@@ -117,7 +118,8 @@ const ClassroomListingpage = () => {
                             <thead>
                               <tr className="bg-gray-100">
                                 <th className="py-2 px-4 text-left">Name</th>
-                                <th className="py-2 px-4 text-left">Roll No</th>
+                                <th className="py-2 px-4 text-left">Email</th>
+                                <th className="py-2 px-4 text-left">Contact</th>
                               </tr>
                             </thead>
                             <tbody>
