e256c3dce14ae7e422d06b6a51124bce437507af
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2024-12-01 10:26:14 +0530

    added pages for page not found and to login before accessing portal

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,8 @@ import CommonLogin from "./Pages/InitialPages/CommonLogin.jsx";
 import TeacherLogin from "./Pages/InitialPages/TeacherLogin.jsx";
 import StudentLogin from "./Pages/InitialPages/StudentLogin.jsx";
 import { useEffect } from "react";
+import LoginAlert from "./components/LoginAlert.jsx";
+import PageNotFound from "./components/PageNotFound.jsx"
 
 
 function App() {
@@ -50,7 +52,6 @@ function App() {
           /* Admin Page Routes */
           savedAuth && savedAuth.role == 'admin' ? 
             <>
-              <Route path="/" element = {<Initial/>}/>
               <Route path='/admin' element={<AdminHome/>}   /> 
               <Route path="/admin/students" element={<StudentListingpage />} />
               <Route path="/admin/teachers" element={<TeacherListingpage />} />
@@ -59,7 +60,15 @@ function App() {
               <Route path="/admin/data-analytics" element={<Dataanalyticspage />} />
               <Route path='/admin/issues' element={<Issuessection/>} />
             </> :
-            <></>
+            <>
+              <Route path='/admin' element={<LoginAlert/>}   /> 
+              <Route path="/admin/students" element={<LoginAlert />} />
+              <Route path="/admin/teachers" element={<LoginAlert />} />
+              <Route path="/admin/classrooms" element={<LoginAlert/>} />
+              <Route path="/admin/classrooms/:id" element={<LoginAlert/>} />
+              <Route path="/admin/data-analytics" element={<LoginAlert />} />
+              <Route path='/admin/issues' element={<LoginAlert/>} />
+            </>
         }
         
         {
@@ -73,7 +82,14 @@ function App() {
             <Route path='/student/pending-homework' element={<PendingHomeWork/>}   /> 
             <Route path='/student/attendance' element={<StudentAttendancePage/>}   /> 
           </> :
-          <></>
+          <>
+            <Route path='/student' element={<LoginAlert/>}   /> 
+            <Route path='/student/submit-homework' element={<LoginAlert/>}/> 
+            <Route path='/student/notification' element={<LoginAlert/>}   /> 
+            <Route path='/student/doubts' element={<LoginAlert/>}   /> 
+            <Route path='/student/pending-homework' element={<LoginAlert/>}   /> 
+            <Route path='/student/attendance' element={<LoginAlert/>}   /> 
+          </>
         }
   
         {
@@ -88,7 +104,15 @@ function App() {
             <Route path="/teacher/classrooms" element={<TeachersClassroom/>}/>
             <Route path='/teacher/generate-issue' element={<GenerateTeacherIssue />} />
           </> : 
-          <></>
+          <>
+            <Route path='/teacher' element={<LoginAlert />} />
+            <Route path='/teacher/signup' element={<LoginAlert />} />
+            <Route path='/teacher/profile' element={<LoginAlert />} />
+            <Route path='/teacher/add-homework' element={<LoginAlert />} />
+            <Route path='/teacher/mark-attendance' element={<LoginAlert />} /> 
+            <Route path="/teacher/classrooms" element={<LoginAlert/>}/>
+            <Route path='/teacher/generate-issue' element={<LoginAlert />} />
+          </>
         }
         
         
@@ -102,6 +126,8 @@ function App() {
         <Route path="/login/admin" element={<AdminLoginpage/>} />
         <Route path="/login/teacher" element={<TeacherLogin/>} />
         <Route path="/login/student" element={<StudentLogin/>}/>
+
+        <Route path="*" element={<PageNotFound />}/>
         
       </Routes>
       <Toaster/>
