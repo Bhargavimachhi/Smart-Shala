9ccf4fa7c8cc9d4b213cc1080c53ec8c2ed4f508
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2024-12-03 00:35:58 +0530

    fixed formatting of login routes

--- src/App.jsx
+++ src/App.jsx
@@ -48,72 +48,32 @@ function App() {
   
       <Routes>
 
-        {
-          /* Admin Page Routes */
-          savedAuth && savedAuth.role == 'admin' ? 
-            <>
-              <Route path='/admin' element={<AdminHome/>}   /> 
-              <Route path="/admin/students" element={<StudentListingpage />} />
-              <Route path="/admin/teachers" element={<TeacherListingpage />} />
-              <Route path="/admin/classrooms" element={<ClassroomListingpage/>} />
-              <Route path="/admin/classrooms/:id" element={<Classroom/>} />
-              <Route path="/admin/data-analytics" element={<Dataanalyticspage />} />
-              <Route path='/admin/issues' element={<Issuessection/>} />
-            </> :
-            <>
-              <Route path='/admin' element={<LoginAlert/>}   /> 
-              <Route path="/admin/students" element={<LoginAlert />} />
-              <Route path="/admin/teachers" element={<LoginAlert />} />
-              <Route path="/admin/classrooms" element={<LoginAlert/>} />
-              <Route path="/admin/classrooms/:id" element={<LoginAlert/>} />
-              <Route path="/admin/data-analytics" element={<LoginAlert />} />
-              <Route path='/admin/issues' element={<LoginAlert/>} />
-            </>
-        }
-        
-        {
-          /* Student Page Routes */
-          savedAuth && savedAuth.role == 'student' ?
-          <>
-            <Route path='/student' element={<StudentHomePage/>}   /> 
-            <Route path='/student/submit-homework' element={<SubmitHomework/>}/> 
-            <Route path='/student/notification' element={<StudentNotification/>}   /> 
-            <Route path='/student/doubts' element={<StudentChatbot/>}   /> 
-            <Route path='/student/pending-homework' element={<PendingHomeWork/>}   /> 
-            <Route path='/student/attendance' element={<StudentAttendancePage/>}   /> 
-          </> :
-          <>
-            <Route path='/student' element={<LoginAlert/>}   /> 
-            <Route path='/student/submit-homework' element={<LoginAlert/>}/> 
-            <Route path='/student/notification' element={<LoginAlert/>}   /> 
-            <Route path='/student/doubts' element={<LoginAlert/>}   /> 
-            <Route path='/student/pending-homework' element={<LoginAlert/>}   /> 
-            <Route path='/student/attendance' element={<LoginAlert/>}   /> 
-          </>
-        }
+        {/* Admin Page Routes */ }
+        <Route path='/admin' element={savedAuth && savedAuth.role == 'admin' ? <AdminHome/> : <LoginAlert />}   /> 
+        <Route path="/admin/students" element={savedAuth && savedAuth.role == 'admin' ?<StudentListingpage />: <LoginAlert />} />
+        <Route path="/admin/teachers" element={savedAuth && savedAuth.role == 'admin' ?<TeacherListingpage />: <LoginAlert />} />
+        <Route path="/admin/classrooms" element={savedAuth && savedAuth.role == 'admin' ?<ClassroomListingpage/>: <LoginAlert />} />
+        <Route path="/admin/classrooms/:id" element={savedAuth && savedAuth.role == 'admin' ?<Classroom/>: <LoginAlert />} />
+        <Route path="/admin/data-analytics" element={savedAuth && savedAuth.role == 'admin' ?<Dataanalyticspage />: <LoginAlert />} />
+        <Route path='/admin/issues' element={savedAuth && savedAuth.role == 'admin' ?<Issuessection/>: <LoginAlert />} />
+
+      
+        {/* Student Page Routes  */}
+        <Route path='/student' element={savedAuth && savedAuth.role == 'student' ? <StudentHomePage/> : <LoginAlert />}   /> 
+        <Route path='/student/submit-homework' element={savedAuth && savedAuth.role == 'student' ? <SubmitHomework/> : <LoginAlert />}/> 
+        <Route path='/student/notification' element={savedAuth && savedAuth.role == 'student' ? <StudentNotification/> : <LoginAlert />}   /> 
+        <Route path='/student/doubts' element={savedAuth && savedAuth.role == 'student' ? <StudentChatbot/> : <LoginAlert />}   /> 
+        <Route path='/student/pending-homework' element={savedAuth && savedAuth.role == 'student' ? <PendingHomeWork/> : <LoginAlert />}   /> 
+        <Route path='/student/attendance' element={savedAuth && savedAuth.role == 'student' ? <StudentAttendancePage/> : <LoginAlert />}   />
   
-        {
-          /* Teacher Page Routes */
-          savedAuth && savedAuth.role == 'teacher' ?
-          <>
-            <Route path='/teacher' element={<TeacherHomePage />} />
-            <Route path='/teacher/signup' element={<TeacherSignUpForm />} />
-            <Route path='/teacher/profile' element={<TeacherProfilePage />} />
-            <Route path='/teacher/add-homework' element={<AddHomeworkPage />} />
-            <Route path='/teacher/mark-attendance' element={<MarkTeacherAttendance />} /> 
-            <Route path="/teacher/classrooms" element={<TeachersClassroom/>}/>
-            <Route path='/teacher/generate-issue' element={<GenerateTeacherIssue />} />
-          </> : 
-          <>
-            <Route path='/teacher' element={<LoginAlert />} />
-            <Route path='/teacher/signup' element={<LoginAlert />} />
-            <Route path='/teacher/profile' element={<LoginAlert />} />
-            <Route path='/teacher/add-homework' element={<LoginAlert />} />
-            <Route path='/teacher/mark-attendance' element={<LoginAlert />} /> 
-            <Route path="/teacher/classrooms" element={<LoginAlert/>}/>
-            <Route path='/teacher/generate-issue' element={<LoginAlert />} />
-          </>
-        }
+        {/* Teacher Page Routes */}
+        <Route path='/teacher' element={savedAuth && savedAuth.role == 'teacher' ? <TeacherHomePage /> : <LoginAlert />} />
+        <Route path='/teacher/signup' element={savedAuth && savedAuth.role == 'teacher' ? <TeacherSignUpForm /> : <LoginAlert />} />
+        <Route path='/teacher/profile' element={savedAuth && savedAuth.role == 'teacher' ? <TeacherProfilePage /> : <LoginAlert />} />
+        <Route path='/teacher/add-homework' element={savedAuth && savedAuth.role == 'teacher' ? <AddHomeworkPage /> : <LoginAlert />} />
+        <Route path='/teacher/mark-attendance' element={savedAuth && savedAuth.role == 'teacher' ? <MarkTeacherAttendance /> : <LoginAlert />} /> 
+        <Route path="/teacher/classrooms" element={savedAuth && savedAuth.role == 'teacher' ? <TeachersClassroom/> : <LoginAlert />}/>
+        <Route path='/teacher/generate-issue' element={savedAuth && savedAuth.role == 'teacher' ? <GenerateTeacherIssue /> : <LoginAlert />} />
         
         
         {/* SignUp Page Routes */}
