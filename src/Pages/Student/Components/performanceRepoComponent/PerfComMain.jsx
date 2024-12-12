d0fafbb6ac89aea21919985434d8f568876d1a29
AaishwaryaSheth <avsheth2005@gmail.com>
2024-12-12 16:17:23 +0530

    student

--- src/Pages/Student/Components/performanceRepoComponent/PerfComMain.jsx
+++ src/Pages/Student/Components/performanceRepoComponent/PerfComMain.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PerfComMain = ({ activeTab, setActiveTab }) => {
   return (
     <div className="p-4 flex gap-4 border-b-2">
-      <button
+      {/* <button
         onClick={() => setActiveTab('overall performance')}
         className={`cursor-pointer px-4 py-2 text-lg ${
           activeTab === 'overall performance'
@@ -12,18 +12,18 @@ const PerfComMain = ({ activeTab, setActiveTab }) => {
         }`}
       >
         Overall Performance
-      </button>
+      </button> */}
       <button
         onClick={() => setActiveTab('evaluate test')}
         className={`cursor-pointer px-4 py-2 text-lg ${
           activeTab === 'evaluate test'
-            ? 'text-sky-800 border-b-2 border-sky-800'
+            ? 'text-sky-800 border-b-2 border-sky-800 font-bold text-2xl'
             : 'text-gray-600'
         }`}
       >
-        Evaluate
+        Evaluate 
       </button>
-      <button
+      {/* <button
         onClick={() => setActiveTab('test results')}
         className={`cursor-pointer px-4 py-2 text-lg ${
           activeTab === 'test results'
@@ -32,7 +32,7 @@ const PerfComMain = ({ activeTab, setActiveTab }) => {
         }`}
       >
         Test Result
-      </button>
+      </button> */}
     </div>
   );
 };
